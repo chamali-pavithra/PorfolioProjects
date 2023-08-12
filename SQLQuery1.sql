04702e66c7a669bf04598730f20d21edf0012897
chamali-pavithra <141349476+chamali-pavithra@users.noreply.github.com>
2023-08-12 18:30:08 +0530

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,78 +1,75 @@
--kk
-select *
-from PortfolioProject_1..covid_deaths
-order by 3,4
+--This quary is bASed on the covid data sourced FROM ourworldindata.org site. The used data sheet include details LIKE, covid cASes,
+--number of deaths and vaccination data.
 
---select *
---from PortfolioProject_1..covid_vaccinations
---order by 3,4
+SELECT *
+FROM PortfolioProject_1..covid_deaths
+ORDER BY 3,4
+	
+--SELECT the data which is need to working with
 
---select data that we are going to be using
+SELECT location,date,total_cASes,new_cASes,total_deaths,population
+FROM PortfolioProject_1..covid_deaths
+ORDER BY 1,2
 
-select location,date,total_cases,new_cases,total_deaths,population
-from PortfolioProject_1..covid_deaths
-order by 1,2
+--Looking at toal cASes vs total deaths
+--Shows the posibility of dying if someone contract covid in his or her country
 
---Looking at toal cases vs total deaths
---Shows the posibility of dying if you contract covid in your country
+SELECT location,date,total_cASes,total_deaths, (total_deaths/total_cASes)*100 AS DeathPersentage
+FROM PortfolioProject_1..covid_deaths
+WHERE location LIKE '%states%'
+ORDER BY 1,2
 
-select location,date,total_cases,total_deaths, (total_deaths/total_cases)*100 AS DeathPersentage
-from PortfolioProject_1..covid_deaths
-Where location like '%states%'
-order by 1,2
+SELECT location,date,total_cASes,total_deaths, (total_deaths/total_cASes)*100 AS DeathPersentage
+FROM PortfolioProject_1..covid_deaths
+WHERE location LIKE '%sri lanka%'
+ORDER BY 1,2
 
-select location,date,total_cases,total_deaths, (total_deaths/total_cases)*100 AS DeathPersentage
-from PortfolioProject_1..covid_deaths
-Where location like '%sri lanka%'
-order by 1,2
+--Looking at total cASes vs population
 
---Looking at total cases vs population
+SELECT location,date,population,total_cASes, (total_cASes/population)*100 AS DeathPersentage
+FROM PortfolioProject_1..covid_deaths
+WHERE location LIKE '%sri lanka%'
+ORDER BY 1,2
 
-select location,date,population,total_cases, (total_cases/population)*100 AS DeathPersentage
-from PortfolioProject_1..covid_deaths
-Where location like '%sri lanka%'
-order by 1,2
+--Looking at total cASes vs population in the world
 
---Looking at total cases vs population in the world
-
-select location,date,population,total_cases, (total_cases/population)*100 AS PercentPopulationInfected
-from PortfolioProject_1..covid_deaths
-order by 1,2
+SELECT location,date,population,total_cASes, (total_cASes/population)*100 AS PercentPopulationInfected
+FROM PortfolioProject_1..covid_deaths
+ORDER BY 1,2
 
 --Looking at countries with highest infection rate compared to population
 
-select location, population,Max(total_cases) as HighestInfectionCount, MAX((total_cases/population)*100) AS PercentPopulationInfected
-from PortfolioProject_1..covid_deaths
-Group by location, population
-order by PercentPopulationInfected desc
+SELECT location, population,Max(total_cASes) AS HighestInfectionCount, MAX((total_cASes/population)*100) AS PercentPopulationInfected
+FROM PortfolioProject_1..covid_deaths
+GROUP BY location, population
+ORDER BY PercentPopulationInfected DESC
 
 --Showing countries with highest death count per population
 
-select location, MAX(total_deaths) AS TotalDeathCount
-from PortfolioProject_1..covid_deaths
-WHERE continent is not null
-Group by location
-order by TotalDeathCount desc
+SELECT location, MAX(total_deaths) AS TotalDeathCount
+FROM PortfolioProject_1..covid_deaths
+WHERE continent IS NOT NULL
+GROUP BY location
+ORDER BY TotalDeathCount DESC
 
 --Break things down by continent
 --Showing continents with the highest death count per population
 
-select continent, MAX(total_deaths) AS TotalDeathCount
-from PortfolioProject_1..covid_deaths
-WHERE continent is not null
-Group by continent
-order by TotalDeathCount desc
+SELECT continent, MAX(total_deaths) AS TotalDeathCount
+FROM PortfolioProject_1..covid_deaths
+WHERE continent IS NOT NULL
+GROUP BY continent
+ORDER BY TotalDeathCount DESC
 
 --Global numbers
 
-select date, SUM(new_cases) AS Total_Cases, SUM(new_deaths) AS Total_Deaths, SUM(CAST(new_deaths AS INT))/SUM(new_cases)*100 AS DeathPersentage
-from PortfolioProject_1..covid_deaths
-WHERE continent is not null  AND new_deaths >0 AND new_cases >0 AND new_deaths is not null AND new_cases is not null
-Group by date
-order by 1,2
+SELECT date, SUM(new_cASes) AS Total_CASes, SUM(new_deaths) AS Total_Deaths, SUM(CAST(new_deaths AS INT))/SUM(new_cASes)*100 AS DeathPersentage
+FROM PortfolioProject_1..covid_deaths
+WHERE continent IS NOT NULL  AND new_deaths >0 AND new_cASes >0 AND new_deaths IS NOT NULL AND new_cASes IS NOT NULL
+GROUP BY date
+ORDER BY 1,2
 
---Retrive data from Covid_Vaccination sheet
-
+--Retrive data FROM Covid_Vaccination sheet
 --Looking at total population vs vaccinations
 
  SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
@@ -102,8 +99,7 @@ ORDER BY 2,3
 
 WITH PopvsVac (continent, location,date,population,new_vaccinations,RollongPeopleVaccinated)
 AS
-(
-SELECT 
+(SELECT 
 	dea.continent, 
 	dea.location, 
 	dea.date, 
@@ -123,12 +119,9 @@ SELECT*, (RollongPeopleVaccinated/population)*100
 FROM PopvsVac
 
 
-
-
-
 --Temporary table
 
-DROP table if exists #PercentopulationVaccinatedd
+DROP TABLE IF EXISTS #PercentopulationVaccinatedd
 CREATE TABLE #PercentopulationVaccinatedd
 (
 
@@ -139,8 +132,6 @@ Population numeric,
 new_vaccinations numeric,
 RollongPeopleVaccinated numeric
 )
-
-
 
 INSERT  #PercentopulationVaccinatedd
 SELECT 
